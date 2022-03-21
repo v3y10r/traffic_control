b353f1a578cfd464a6a368e2ad34ab28d39feb32
v3y10r <65041505+v3y10r@users.noreply.github.com>
2022-03-21 12:13:18 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-
 namespace traffic
 {
     class Program
     {
-        class car
+        abstract class car
         {
             private protected string location, destination, id;
             private protected int waiting_time, priority;
@@ -30,9 +29,9 @@ namespace traffic
                     priority = 999;
                 }
             }
-            public void print_info()
+            public virtual void print_info()
             {
-                Console.WriteLine($"Car {id} with priority lvl {priority} is located at {location} and needs to get to {destination}, its waiting time is {waiting_time}");
+                Console.WriteLine($"Car {id} is located at {location} and needs to get to {destination}, its waiting time is {waiting_time}");
             }
             public void travel()
             {
@@ -46,27 +45,69 @@ namespace traffic
                 waiting_time += s;
                 Console.WriteLine($"Car {id} waits for {s} seconds");
             }
-        }
-        class passenger : car
-        {
-            public passenger(string l, string d, int t, string i) : this(l, d, t, i, 0) { }
-            public passenger(string l, string d, int t, string i, int p) : base(l, d, t, i)
+            public void wait(int s, car second)
             {
-                priority = p;
+                waiting_time += s;
+                Console.WriteLine($"Car {id} waits before {second.id} for {s} seconds");
             }
-
+            public int waiting_time_output()
+            {
+                return waiting_time;
+            }
+            public int waiting_time_output(bool is_idle)
+            {
+                if (!is_idle)
+                    return waiting_time;
+                else
+                    return waiting_time + 5;
+            }
+            public static int operator >(car car1, car car2)
+            {
+                car 
+            }
         }
         class cargo : car
         {
-            public cargo(string l, string d, int t, string i) : this(l, d, t, i, 1) { }
-            public cargo(string l, string d, int t, string i, int p) : base(l, d, t, i)
+            private protected string load;
+            public cargo(string l) : this(l, "none") { }
+            public cargo(string l, string d) : this(l, d, 0) { }
+            public cargo(string l, string d, int t) : this(l, d, t, "undefined") { }
+            public cargo(string l, string d, int t, string i) : this(l, d, t, i, "no_load") { }
+            public cargo(string l, string d, int t, string i, string load) : base(l, d, t, i) 
             {
-                priority = p;
+                priority = 1;
+            }
+            public override void print_info()
+            {
+                Console.WriteLine($"Cargo carrier {id} with {load} payload is located at {location} and needs to get to {destination}, its waiting time is {waiting_time}");
+            }
+            public void travel(bool is_idle)
+            {
+                if (is_idle)
+                    this.wait(5);
+                Console.WriteLine($"Car {id} travels from {location} to {destination}");
+                location = destination;
+                destination = "none";
+                waiting_time = 0;
+            }
+        }
+        class passenger : car
+        {
+            public passenger(string l) : this(l, "none") { }
+            public passenger(string l, string d) : this(l, d, 0) { }
+            public passenger(string l, string d, int t) : this(l, d, t, "undefined") { }
+            public passenger(string l, string d, int t, string i) : base(l, d, t, i)
+            {
+                priority = 0;
+            }
+            public override void print_info()
+            {
+                Console.WriteLine($"Passenger car {id} is located at {location} and needs to get to {destination}, its waiting time is {waiting_time}");
             }
         }
         static void Main(string[] args)
         {
-            car test = new car("south", "north", 5);
+            car test = new cargo("south", "north", 5);
             test.print_info();
             test.wait(15);
             test.print_info();
